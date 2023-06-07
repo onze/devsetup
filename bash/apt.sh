55190a1bb8ea480707b59b2b41a3e04d7ef43abd
val <vbisson@aurorasolar.com>
2023-06-06 22:07:49 -0400

    split bashrc_common

---
+++ bash/apt.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+alias i='sudo apt install -y'
+alias r='sudo apt remove'
+alias s='apt-cache search'
+alias update='sudo apt update'
+alias upgrade='sudo apt upgrade'
+alias distupgrade='sudo apt-get full-upgrade'
+alias autoremove='sudo apt-get autoremove'
+alias purge='sudo apt-get purge'
+alias show='apt-cache show'
+alias list='dpkg-query -L'
+alias apt-origin='apt-file search'
---
+++ bash/python.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+alias py='python3'
+alias python='python3'
+alias ipy='ipython3'
+alias opensimplehttpserver='python3 -m http.server 8000'
+alias opensimpleftpserver='python3 -m pyftpdlib'
+alias rmpyc='find . -name "*.pyc" -exec rm -rf {} \;'
